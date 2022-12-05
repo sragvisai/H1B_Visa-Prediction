9eff96747ee6dc53a3ff5aa63afa8383f1fb5bf2
Abhishek Chintalapati <abhishek.chintalapati@gmail.com>
2022-12-05 12:50:37 -0800

    Add files via upload

--- Trial.py
+++ Trial.py
@@ -26,6 +26,7 @@ def returnCompanies():
     blackListedCompanies =  ['Broadgate, Inc.','Spate Business Solutions, LLC','Cloudpoint Systems, Inc','Open Access Technology International, Inc.','Invensys, Inc']
     return {blackListedCompanies}
 
+
 @app.get("/")
 async def predict(input_data : str):
     print("Input data ",input_data) 
@@ -34,19 +35,23 @@ async def predict(input_data : str):
     inputData = inputData.split(",")
     if len(inputData) == 6:
         if inputData[0]:
-            with open('role_names.txt') as myfile:
-                if inputData[0] not in myfile.read():
-                    return json.dumps({"outcome" : "Denied"})
+            with open(r'role_names.txt', 'r') as file:
+                content = file.read()
+                if inputData[0] not in content:
+                    return json.dumps({"outcome" : "Denied-R"})
         if inputData[1]:
-            with open('firm_names.txt') as myfile:
-                if inputData[1] not in myfile.read():
-                    return json.dumps({"outcome" : "Denied"})
+            with open(r'firm_names.txt', 'r') as file:
+                content = file.read()
+                if inputData[1] not in content:
+                    return json.dumps({"outcome" : "Denied-F"})
         if inputData[1] in BLACKLISTEDCOMPANIES:
             return json.dumps({"outcome" : "Denied-B"})
         if int(inputData[2]) < int(inputData[3]):
             return json.dumps({"outcome" : "Denied-S"})
-        if inputData[4] in BLACKLISTEDCOUNTRIES or inputData[4] not in countries:
+        if inputData[4] not in countries:
             return json.dumps({"outcome" : "Denied-C"})
+        if inputData[4] in BLACKLISTEDCOUNTRIES:
+             return json.dumps({"outcome" : "Denied-CB"})
         if inputData[5] != "Yes":
             return json.dumps({"outcome" : "Denied-E"})
         else:
@@ -54,26 +59,26 @@ async def predict(input_data : str):
     
     try:
 
-        with open('model2.pickle', 'rb') as f:
+        with open('model_new.pickle', 'rb') as f:
             model = pickle.load(f)
-        with open('encoder2.pickle', 'rb') as f:
+        with open('encoder_new.pickle', 'rb') as f:
             encoder = pickle.load(f)
       
         if int(inputData[10]) < int(inputData[12]):
                 return json.dumps({"outcome" : "Denied-S"})
-        # inputData = ['Software Developer','1','1','0','0','0','0','Google LLC','Yes','Yes','120000','Year','100000','Year','10','Yes','Yes','$60,000 or higher annual wage']
+        #inputData = ['Software Developer','1','1','0','0','0','0','Google LLC','Yes','Yes','120000','Year','100000','Year','10','Yes','Yes','$60,000 or higher annual wage']
    
-        
+        print("Before Encoding",inputData)
         inputData= np.asarray(inputData)
         inputData = inputData.reshape(1,-1)
 
         inputData = encoder.transform(inputData)
-        
-        print("Reverse Encoding "+encoder.inverse_transform(inputData))
+        print("After Encoding",inputData)
+        print("Reverse Encoding ",encoder.inverse_transform(inputData))
 
         predictedOutcome = model.predict(inputData)
         print("Returning ",predictedOutcome)
-        return json.dumps({"outcome" : (str("replace"[0]))})
+        return json.dumps({"outcome" : (str(predictedOutcome[0]))})
     
     except:
         return json.dumps({"outcome":"ERROR"})
